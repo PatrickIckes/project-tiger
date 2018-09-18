98d4b79756108699b7057550dc9f2a748bb2124c
Patrick Ickes <Patrick.Ickes@iam.local>
2018-09-18 15:18:14 -0500

    Added movement and edited script

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
-
+    public Rigidbody2D myRigidBody;
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log("This is start!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKey(KeyCode.D))
+        {
+            //TODO: Move the character to the right
+            myRigidBody.velocity = new Vector2(5, myRigidBody.velocity.y);
+        }
+        //This is the syntax for printing to the console!
+        //Debug.Log("Test!");
 	}
 }
