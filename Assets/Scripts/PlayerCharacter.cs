fa0dd6a7231ec7b30d8d6995ceb1977c325d877c
Patrick Ickes <Patrick.Ickes@iam.local>
2018-09-25 14:10:56 -0500

    Movement additions

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
-    float movementSpeed = 5;
+    [SerializeField]
+    private float movementSpeed = 5;
 
-    public Rigidbody2D myRigidBody;
+    [SerializeField]
+    private Rigidbody2D myRigidBody;
+
 	// Use this for initialization
 	void Start () {
         Debug.Log("This is start!");
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         if (Input.GetKey(KeyCode.D))
         {
             //TODO: Move the character to the right
+            movementSpeed = 5;
+            myRigidBody.velocity = new Vector2(movementSpeed, myRigidBody.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.W));
+        {
+            //Jumping
+            myRigidBody.velocity = new Vector2(movementSpeed, myRigidBody.velocity.x);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //Move left
+            movementSpeed = -5;
             myRigidBody.velocity = new Vector2(movementSpeed, myRigidBody.velocity.y);
         }
         //This is the syntax for printing to the console!
         //Debug.Log("Test!");
-        //Comment
 	}
 }
