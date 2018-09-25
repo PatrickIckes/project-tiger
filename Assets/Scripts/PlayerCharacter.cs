a17211cb5bca1d668af38640718ec0b75ff31825
Patrick Ickes <Patrick.Ickes@iam.local>
2018-09-25 15:17:50 -0500

    Movement updates

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -9,33 +9,38 @@ public class PlayerCharacter : MonoBehaviour {
     [SerializeField]
     private Rigidbody2D myRigidBody;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private float moveInput;
+
+    // Use this for initialization
+    void Start () {
         Debug.Log("This is start!");
 	}
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKey(KeyCode.D))
-        {
-            //TODO: Move the character to the right
-            movementSpeed = 5;
-            myRigidBody.velocity = new Vector2(movementSpeed, myRigidBody.velocity.y);
-        }
+    void Update()
+    {
+        GetMovementInput();
+    }
 
-        if (Input.GetKey(KeyCode.W));
-        {
-            //Jumping
-            myRigidBody.velocity = new Vector2(movementSpeed, myRigidBody.velocity.x);
-        }
+    private void FixedUpdate()
+    {
+        Move();
+    }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            //Move left
-            movementSpeed = -5;
-            myRigidBody.velocity = new Vector2(movementSpeed, myRigidBody.velocity.y);
-        }
-        //This is the syntax for printing to the console!
-        //Debug.Log("Test!");
-	}
+    private void Move()
+    {
+        myRigidBody.velocity = new Vector2(moveInput * movementSpeed, myRigidBody.velocity.y);
+    }
+
+    private void Jump()
+    {
+        //TODO: Make the character jump
+
+    }
+
+    private void GetMovementInput()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+    }
 }
