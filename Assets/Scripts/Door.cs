ae28b9b6a33c1888adddaa3985ab708299f8349b
PatrickIckes <patrick.ickes@loop.colum.edu>
2018-10-23 15:13:32 -0500

    added test2 scene and scene manager

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,17 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour {
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private bool isPlayerInTrigger;
+
+    // Bug: Triggers door twice due to double colliders on Player
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        if (Input.GetButtonDown("Activate"))
+    //        {
+    //            Debug.Log("Player activated door");
+    //        }
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            if (Input.GetButtonDown("Activate"))
-            {
-                Debug.Log("Player activated door");
-            }
+            isPlayerInTrigger = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Activate") && isPlayerInTrigger)
+        {
+            Debug.Log("Player activated door");
+            SceneManager.LoadScene("Test2");
         }
     }
 }
