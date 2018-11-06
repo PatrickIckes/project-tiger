8dc0d5f1f0f8170c9944e908373e8aaccf32a79b
PatrickIckes <patrick.ickes@loop.colum.edu>
2018-11-06 13:28:45 -0600

    added checkpoint sprites and code

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,59 +5,22 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     [SerializeField]
-    private float inactivatedRotationSpeed = 100, activatedRotationSpeed = 300;
-
-    [SerializeField]
-    private float inactivatedScale = 1, activatedScale = 1.5f;
-
-    [SerializeField]
-    private Color inactivatedColor, activatedColor;
+    private Sprite activatedSprite;
 
     private bool isActivated;
     private SpriteRenderer spriteRenderer;
-    private AudioSource audioSource;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        audioSource = GetComponent<AudioSource>();
-        UpdateColor();
     }
     private void Update()
     {
-        UpdateRotation();
+        
     }
-    private void UpdateColor()
-    {
-        Color color = inactivatedColor;
-        if (isActivated)
-            color = activatedColor;
-
-        spriteRenderer.color = color;
-    }
-    private void UpdateScale()
-    {
-        float scale = inactivatedScale;
-        if (isActivated)
-            scale = activatedScale;
-
-        transform.localScale = Vector3.one * scale;
-    }
-
-    private void UpdateRotation()
-    {
-        float rotationSpeed = inactivatedRotationSpeed;
-        if (isActivated)
-            rotationSpeed = activatedRotationSpeed;
-
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
-    }
-
     public void SetIsActivated(bool value)
     {
         isActivated = value;
-        UpdateScale();
-        UpdateColor();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -67,7 +30,8 @@ public class Checkpoint : MonoBehaviour
             Debug.Log("Checkpoint!");
             PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
             player.SetCurrentCheckpoint(this);
-            audioSource.Play();
+            spriteRenderer.sprite = activatedSprite;
+
         }
     }
 
