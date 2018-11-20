b2b63f02ceaeacfcad8ee10b4074b553efbbb7a9
PatrickIckes <patrick.ickes@loop.colum.edu>
2018-11-19 21:03:58 -0600

    Added gem script and rotation

---
+++ Assets/GemSpin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpin : MonoBehaviour {
+
+	
+	void Update ()
+    {
+        transform.Rotate(new Vector3(0, 135, 0) * Time.deltaTime);
+	}
+}
